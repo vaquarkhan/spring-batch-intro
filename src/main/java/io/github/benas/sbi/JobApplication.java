3d0d4cad68bd6656860d048eb16a35c5a74b4fe9
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-05-03 23:02:16 +0200

    Turn the app into a Spring Cloud Task app

--- src/main/java/io/github/benas/sbi/JobApplication.java
+++ src/main/java/io/github/benas/sbi/JobApplication.java
@@ -2,7 +2,9 @@ package io.github.benas.sbi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 
+@EnableTask
 @SpringBootApplication
 public class JobApplication {
 
