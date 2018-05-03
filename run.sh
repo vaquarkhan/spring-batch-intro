61479339f69192f00ce97ee2ba0016ff4d5d1d58
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-05-03 16:33:06 +0200

    Use a job parameter to specify the hello message

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 java -cp target/spring-batch-intro-1.0-SNAPSHOT.jar \
   org.springframework.batch.core.launch.support.CommandLineJobRunner \
   io.github.benas.sbi.JobConfiguration \
-  job
+  job \
+  $*
--- src/main/java/io/github/benas/sbi/JobConfiguration.java
+++ src/main/java/io/github/benas/sbi/JobConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -27,12 +30,19 @@ public class JobConfiguration {
 	}
 
 	@Bean
+	@StepScope
+	public Tasklet tasklet(@Value("#{jobParameters['name']}") String name) {
+		return (contribution, chunkContext) -> {
+			System.out.println("hello " + name);
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@Bean
 	public Step step() {
 		return stepBuilderFactory.get("step")
-				.tasklet((contribution, chunkContext) -> {
-					System.out.println("hello world");
-					return RepeatStatus.FINISHED;
-				}).build();
+				.tasklet(tasklet(null))
+				.build();
 	}
 
 	@Bean
