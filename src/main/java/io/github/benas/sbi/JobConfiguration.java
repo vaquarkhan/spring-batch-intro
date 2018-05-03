80503a8b1dcc05f7c5241444cda7fbfad4592422
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-05-03 16:22:32 +0200

    add hello world job definition

--- src/main/java/io/github/benas/sbi/JobConfiguration.java
+++ src/main/java/io/github/benas/sbi/JobConfiguration.java
@@ -1,9 +1,14 @@
 package io.github.benas.sbi;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +44,25 @@ public class JobConfiguration {
 		SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
 		simpleJobLauncher.setJobRepository(jobRepository());
 		return simpleJobLauncher;
+	}
+
+	@Bean
+	public Step step() throws Exception {
+		return new StepBuilder("step")
+				.repository(jobRepository())
+				.transactionManager(transactionManager)
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("hello world");
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+
+	@Bean
+	public Job job() throws Exception {
+		return new JobBuilder("job")
+				.repository(jobRepository())
+				.start(step())
+				.build();
 	}
 
 }
