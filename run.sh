327193d63cbd290134cc02e27b2f0a2fa779adf9
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-05-03 16:29:03 +0200

    Add script to run the job

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+java -cp target/spring-batch-intro-1.0-SNAPSHOT.jar \
+  org.springframework.batch.core.launch.support.CommandLineJobRunner \
+  io.github.benas.sbi.JobConfiguration \
+  job
